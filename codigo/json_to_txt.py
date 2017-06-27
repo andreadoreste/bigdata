e80873a5622d48db926572a1edce37462b28e653
Andrea <andrea.doreste@poli.ufrj.br>
2017-06-26 21:52:33 -0300

    Update json_to_txt and unicode_to_text

--- codigo/json_to_txt.py
+++ codigo/json_to_txt.py
@@ -4,26 +4,39 @@ import json
 import os
 import codecs
 
-newDocument = open('stranger-things_v2.txt','a')
-test = open('stranger-things.json','r')
-#test = codecs.open('stranger-things.json',encoding='utf-8')
-lines = test.readlines()
 
-for line in lines:
-	js = line
-	a = json.loads(js)
-	a = a['text']
-	a = a.replace("\n"," ")
+
+def json_to_txt(list_of_json):
+	
+	for name_json in list_of_json:
+		name_file = name_json.split('.')
+		name_file = name_file[0]
+		print name_file
+		name_file = name_file+'.txt'
+
+		newDocument = open(name_file,'a')
+		test = open(name_json,'r')
+#test = codecs.open('stranger-things.json',encoding='utf-8')
+		lines = test.readlines()
+
+		for line in lines:
+			js = line
+			a = json.loads(js)
+			a = a['text']
+			a = a.replace("\n"," ")
 	#a = a.decode('unicode_escape').encode('ascii','ignore')
 	#a = a.encode('ascii','ignore')
-	a = a.encode('utf-8')
+			a = a.encode('utf-8')
+			#a = a.encode('ascii','ignore')
 	#a = unicode(a,errors='ignore')
 	#print a
 	#a.encode("utf-8")
 #js = lines[0]
 #a =json.loads(js)
 	#print a['text']
-	newDocument.write(a + '\n')
+			newDocument.write(a + '\n')
 	#print "yey"
-newDocument.close()
-test.close()	
+		newDocument.close()
+		test.close()	
+
+#json_to_txt('daredevil.json')
--- codigo/unicode_to_text.py
+++ codigo/unicode_to_text.py
@@ -1,31 +1,43 @@
+# -*- coding: utf-8 -*-
+
 import os
 
-newDoc = open('wordcount_v2\wordcount_stranger-things.txt','a')
-oldDoc0 = open('mRstranger-things\part-00000','r')
-oldDoc1 = open('mRstranger-things\part-00001','r')
+#newDoc = open('wordcount_v2\wordcount_stranger-things.txt','a')
+#oldDoc0 = open('mRstranger-things\part-00000','r')
+#oldDoc1 = open('mRstranger-things\part-00001','r')
 
-list = oldDoc0.readlines()
-l2 = oldDoc1.readlines()
+def unicode_to_txt(name):
+	name_new_doc = name.split('.')
+	name_new_doc = name_new_doc[0]
+	name_new_doc = 'wordcount_' + name_new_doc + '.txt'
+	newDoc = open(name_new_doc,'a')
+
+	oldDoc = open(name,'r')
+
+	l = oldDoc.readlines()
+#l2 = oldDoc1.readlines()
 
 #list.append(oldDoc1.readlines())
 #list = list.replace(')(',');(')
-newList = []
-for i in list:
+	newList = []
+	for i in l:
 
-	i = eval(i)
+		i = eval(i)
 	#i = i.partition(',')
 	
 	#word = i[0].encode('utf-8')
-	word = i[0].encode('ascii','ignore')
-	newDoc.write(word+','+str(i[1])+'\n')
-	
-for j in l2:
+		word = i[0].encode('ascii','ignore')
+		newDoc.write(word+','+str(i[1])+'\n')
+	'''	
+	for j in l2:
 
-	j = eval(j)
-	#word = j[0].encode('utf-8')
-	word = j[0].encode('ascii','ignore')
-	newDoc.write(word+','+str(j[1])+'\n')
+		j = eval(j)
+		#word = j[0].encode('utf-8')
+		word = j[0].encode('ascii','ignore')
+		newDoc.write(word+','+str(j[1])+'\n')
+	'''
+	newDoc.close()
+	oldDoc.close()
+#oldDoc1.close()
 
-newDoc.close()
-oldDoc0.close()
-oldDoc1.close()
+unicode_to_txt('part-00000')
